c7d9c274826d1f12f30f4d34c645a9f0b7a913a3
Khamzat91 <khamzat91@mail.ru>
2023-02-20 21:59:03 +0300

    add block benefits

---
+++ app/js/header.js
@@ -0,0 +1,25 @@
+const mapImg = document.querySelector(".map__inner-scope__map-img");
+
+const tabs = document.querySelector(".map__inner-tabs__list");
+
+const tabsAll = document.querySelectorAll(".map__inner-tabs__area");
+
+const fold = document.querySelector(".map__inner-tabs__fold");
+
+const items = document.querySelector('.map__inner-tabs__items')
+
+tabs.onclick = (e) => {
+  mapImg.src = `./images/map-${e.target.id}.png`;
+  console.log(tabsAll);
+  for (let i = 0; i < tabsAll.length; i++) {
+    tabsAll[i].classList.remove("active");
+  }
+  e.target.classList.add("active");
+};
+
+fold.onclick = () => {
+  fold.classList.toggle("active");
+  items.classList.toggle("dn");
+  tabs.classList.toggle("opacity");
+  mapImg.classList.toggle("opacity");
+};
